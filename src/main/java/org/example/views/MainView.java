250923402f26ae0fc3d94daeccb8721f92d6d6c8
Matti Tahvonen <matti@vaadin.com>
2024-01-25 17:13:19 +0200

    refactoring, cleanup

--- src/main/java/org/example/views/MainView.java
+++ src/main/java/org/example/views/MainView.java
@@ -5,44 +5,40 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.WebStorage;
 import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.Command;
-import org.apache.commons.io.FileUtils;
 import org.vaadin.firitin.components.DynamicFileDownloader;
 import org.vaadin.firitin.components.RichText;
 import org.vaadin.firitin.components.button.VButton;
 import org.vaadin.firitin.components.orderedlayout.VHorizontalLayout;
 import org.vaadin.firitin.components.upload.UploadFileHandler;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.Base64;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 @Route
 public class MainView extends VerticalLayout {
+    private Spreadsheet spreadsheet;
+
     private final Button newItem = new VButton("New...", this::createNew)
             .withTooltip("Creates a new spreadsheet");
     private final Button closeItem = new VButton("Close", this::close);
     private final UploadFileHandler openItem = new UploadFileHandler(
-                (i, d) -> openExcellFile(i))
+                (inputStream, metadata) -> {
+                    spreadsheet = new Spreadsheet(inputStream);
+                    return () -> displaySpreadsheet(); // Execute later in UI thread
+                })
+            // make browser accept only relevant files
             .withAcceptedFileTypes(".xls", ".xlsx", "application/vnd.ms-excel")
-            // make the file upload very compact by using
-            // icon and disabling drag and drop area
+            // disable d&d, icon only -> just a compact icon in toolbar
             .withDragAndDrop(false)
             .withUploadButton(new VButton(VaadinIcon.UPLOAD.create())
                     .withTooltip("Upload .xslx file...")
             );
 
-    private final DynamicFileDownloader saveItem = new DynamicFileDownloader(this::writeXlsFile)
-            .withTooltip("Downloads current file to disk...");
+    private final DynamicFileDownloader saveItem = new DynamicFileDownloader(
+            out -> spreadsheet.write(out)
+    ).withTooltip("Downloads current file to disk...");
 
     private final Button saveToWebStorage = new VButton(
             VaadinIcon.CLOUD_DOWNLOAD.create(), this::saveToWebStorage)
@@ -50,20 +46,22 @@ public class MainView extends VerticalLayout {
     private final Button openFromWebStorage = new VButton(
                 VaadinIcon.CLOUD_UPLOAD.create(), this::openFromWebStorage)
             .withTooltip("Open previously saved file from browsers local storage...");
-    private Spreadsheet spreadsheet;
 
     private Component about = new RichText().withMarkDown("""
-Welcome to e-xell! 
-
-This is not a good app, don't use if for anything real. 
-
-This is demo app. Check the [GitHub page](https://github.com/mstahv/e-xell) why this app exists.
-""");
+        # Welcome to e-xell!
+        
+        This is not a good app, don't use if for anything real. 
+        
+        This is demo app. Check the [GitHub page](https://github.com/mstahv/e-xell) why this app exists.
+        """);
 
 
     public MainView() {
+        // Initial state for toolbar buttons
         Arrays.asList(saveItem, saveToWebStorage, closeItem).forEach(i -> i.setEnabled(false));
+        // create toolbar
         add(new VHorizontalLayout(newItem, openItem, openFromWebStorage, saveItem, saveToWebStorage, closeItem));
+        // add about text/disclaimer
         add(about);
     }
 
@@ -91,47 +89,26 @@ This is demo app. Check the [GitHub page](https://github.com/mstahv/e-xell) why 
         Arrays.asList(openItem, openFromWebStorage, newItem).forEach(i -> i.setEnabled(!fileOpen));
     }
 
-    public void writeXlsFile(OutputStream out) throws IOException {
-            spreadsheet.write(out);
-    }
-
-    public Command openExcellFile(InputStream content) throws IOException {
-        spreadsheet = new Spreadsheet(content);
-        return () -> displaySpreadsheet(); // Execute later in UI thread
-    }
-
     public void saveToWebStorage() {
-        WebStorageFilePicker.pickNewOrExistingFile().thenAccept(filename -> {
+        WebStorageFilePicker.writeToNewOrExistingFile(outputStream -> {
             try {
-                ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                GZIPOutputStream out = new GZIPOutputStream(bout);
-                spreadsheet.write(out);
-                String data = Base64.getEncoder().encodeToString(bout.toByteArray());
-                WebStorage.setItem(filename, data);
-                String sizeStr = FileUtils.byteCountToDisplaySize(data.length());
-                Notification.show("Saved %s to your browsers local storage.".formatted(sizeStr));
+                spreadsheet.write(outputStream);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
-
+        Notification.show("File saved to your browsers local storage!");
     }
 
     public void openFromWebStorage() {
-        WebStorageFilePicker.pickExisting().thenAccept(filename -> {
-            WebStorage.getItem(filename, data -> {
-                try {
-                    var input = new GZIPInputStream(
-                            new ByteArrayInputStream(
-                                    Base64.getDecoder().decode(data)));
-                    openExcellFile(input).execute();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-            });
+        WebStorageFilePicker.readFromExistingFile(input -> {
+            try {
+                spreadsheet = new Spreadsheet(input);
+                displaySpreadsheet();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
-
     }
 
 }
--- src/main/java/org/example/views/WebStorageFilePicker.java
+++ src/main/java/org/example/views/WebStorageFilePicker.java
@@ -13,14 +13,42 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import org.vaadin.firitin.components.button.VButton;
 import org.vaadin.firitin.components.orderedlayout.VHorizontalLayout;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
+/**
+ * A general purpose "file picker" to web storage (local storage).
+ * Allows to pick existing, existing or new files and save arbitrary
+ * binary data to the web storage.
+ * <p>
+ *     The component uses key "files" to maintain the list of used
+ *     "file names". The "files" are then just mappings of those
+ *     "file names". If the file contents is binary data, it is filtered
+ *     through Gzip and Base64 to be stored to web storage.
+ * </p>
+ */
 public class WebStorageFilePicker {
 
+    /**
+     * Lets the user pick an existing "file" from Web Storage or type in a name for a new one.
+     * Newly created names are stored in local storage as well, so they will be available
+     * if using this helper class again.
+     *
+     * @return the name of the "file" in web storage as {@link CompletableFuture}. Available when user has picked
+     * existing or defined a new name.
+     */
     public static CompletableFuture<String> pickNewOrExistingFile() {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
 
@@ -67,6 +95,12 @@ public class WebStorageFilePicker {
         return completableFuture;
     }
 
+    /**
+     * Shows a dialog to pick existing "file" stored in the web storage.
+     * Use this to implement "File->Open" to your web app.
+     *
+     * @return the filename as {@link CompletableFuture} available when user has selected it.
+     */
     public static CompletableFuture<String> pickExisting() {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
 
@@ -96,5 +130,56 @@ public class WebStorageFilePicker {
 
         return completableFuture;
     }
+
+    public static void write(String fileName, Consumer<OutputStream> writeTask) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        try {
+            GZIPOutputStream out = new GZIPOutputStream(bout);
+            writeTask.accept(out);
+            // WebStorage is string-string, base64 encode to String
+            String data = Base64.getEncoder().encodeToString(bout.toByteArray());
+            // Save to WebStorage and notify user
+            WebStorage.setItem(fileName, data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Picks a new or existing "file" from the web storage and writes data
+     * to it.
+     *
+     * @param writeTask the task that writes the data once user has picked the
+     *                  file.
+     */
+    public static void writeToNewOrExistingFile(Consumer<OutputStream> writeTask) {
+        pickNewOrExistingFile().thenAccept(filename -> {
+            write(filename, writeTask);
+        });
+    }
+
+    /**
+     * Let's the user pick an existing file whose content is then passed
+     * for given callback.
+     *
+     * @param readTask the task to handle the file contents after user has
+     *                 picked and existing file.
+     */
+    public static void readFromExistingFile(Consumer<InputStream> readTask) {
+        WebStorageFilePicker.pickExisting().thenAccept(filename -> {
+            WebStorage.getItem(filename, data -> {
+                try {
+                    var input = new GZIPInputStream(
+                            new ByteArrayInputStream(
+                                    Base64.getDecoder().decode(data)));
+                    readTask.accept(input);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        });
+    }
+
 
 }
